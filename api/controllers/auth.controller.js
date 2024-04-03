017538724f0c72a5fd94d39ea768ffd14bb8a72e
rdx.develops <rdx.develops@gmail.com>
2024-04-03 17:29:52 +0530

    added Google OAuth functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -42,11 +42,52 @@ export const signin = async (req, res, next) => {
       expiresIn: "12h",
     });
 
-    const { password: userPassword, ...rest } =  user._doc;
+    const { password: userPassword, ...rest } = user._doc;
     res
       .status(200)
       .cookie("access_token", token, { httpOnly: true })
       .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const google = async (req, res, next) => {
+  const { username, email, profilePicture } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+        expiresIn: "12h",
+      });
+      const { password: userPassword, ...rest } = user._doc;
+      res
+        .status(200)
+        .cookie("access_token", token, { httpOnly: true })
+        .json(rest);
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+      const newUser = new User({
+        username:
+          username.split(" ").join("").toLowerCase() +
+          Math.random().toString(9).slice(-4),
+        email,
+        password: generatedPassword,
+        profilePicture,
+      });
+      await newUser.save();
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+        expiresIn: "12h",
+      });
+      const { password: userPassword, ...rest } = newUser._doc;
+      res
+        .status(200)
+        .cookie("access_token", token, { httpOnly: true })
+        .json(rest);
+    }
   } catch (error) {
     next(error);
   }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { signup, signin } from "../controllers/auth.controller.js";
+import { signup, signin, google } from "../controllers/auth.controller.js";
 
 const router = Router();
 
 router.route("/signup").post(signup);
 router.route("/signin").post(signin);
+router.route('/google').post(google)
 
 export default router
