bd968b284c04cfdd06d92036ff0770d1bde0e7ec
rdx.develops <rdx.develops@gmail.com>
2024-04-07 13:05:39 +0530

    completed sidebar of the dashboard

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,38 @@
+import {
+  Sidebar,
+  SidebarItem,
+  SidebarItemGroup,
+  SidebarItems,
+} from "flowbite-react";
+import { useEffect, useState } from "react";
+import { HiUser, HiArrowSmRight } from "react-icons/hi";
+import { Link, useLocation } from "react-router-dom";
+export default function DashSidebar() {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (
+    <Sidebar className="w-full md:w-56">
+      <SidebarItems>
+        <SidebarItemGroup>
+          <Link to='/dashboard?tab=profile'>
+            <SidebarItem
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"User"}
+              labelColor="dark">
+              Profile
+            </SidebarItem>
+          </Link>
+          <SidebarItem icon={HiArrowSmRight}>Sign Out</SidebarItem>
+        </SidebarItemGroup>
+      </SidebarItems>
+    </Sidebar>
+  );
+}
