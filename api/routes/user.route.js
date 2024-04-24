ec8fb01ee26afc144edce8b8996544ed38af9982
rdx.develops <rdx.develops@gmail.com>
2024-04-24 19:17:33 +0530

    added getusers api route  accessible by only admins

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import { deleteUser, updateUser, signout } from "../controllers/user.controller.js";
+import {
+  deleteUser,
+  updateUser,
+  signout,
+  getUsers,
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = Router();
 
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
-router.post('/signout', signout)
+router.post("/signout", signout);
+router.get("/getusers", verifyToken, getUsers);
 
 export default router;
