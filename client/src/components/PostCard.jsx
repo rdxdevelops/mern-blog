f5581b9e1a9e58fcf58617d1905bc1787962a29d
rdx.develops <rdx.develops@gmail.com>
2024-05-07 18:59:14 +0530

    fix size of the PostCard component

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 export default function PostCard({ post }) {
   return (
-    <div className="group relative w-full border border-teal-500 hover:border-2 h-[350px] overflow-hidden rounded-lg sm:w-[430px] transition-all">
+    <div className="group relative w-full border border-teal-500 hover:border-2 h-[380px] overflow-hidden rounded-lg sm:w-[360px] transition-all">
       <Link to={`/post/${post.slug}`}>
         <img
           src={post.image}
@@ -14,6 +14,7 @@ export default function PostCard({ post }) {
         <span className="italic text-sm">{post.category}</span>
         <Link
           to={`/post/${post.slug}`}
+          state={post}
           className="z-[10] group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2">
           Read Article
         </Link>
